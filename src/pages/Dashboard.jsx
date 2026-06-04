df68214273542ebdb62e877e2034ac08fd7407f5
2381058@unai.edu <2381058@unai.edu>
2026-06-04 10:03:18 +0700

    memperbaiki semua error yang ada

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,6 +14,7 @@ import {
   Bell
 } from 'lucide-react';
 import Modal from '../components/common/Modal';
+import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
 
 const Dashboard = () => {
   const navigate = useNavigate();
